1b6294a226238bdd98ca72b81e6bbf540bdbedaa
Alyssa Tamayo <atamayo@pdx.edu>
2020-10-06 09:05:26 -0700

    deploy to gh user page

---
+++ precache-manifest.7013adcc3b2d7b5235da980ff86525ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49dd883a116c52dc8487a6805fa29575",
+    "url": "/index.html"
+  },
+  {
+    "revision": "590d65abd6dd66c769cd",
+    "url": "/static/css/main.56bf3105.chunk.css"
+  },
+  {
+    "revision": "77178f69c0376156e6a3",
+    "url": "/static/js/2.d094fe17.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.d094fe17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "590d65abd6dd66c769cd",
+    "url": "/static/js/main.5227d0b5.chunk.js"
+  },
+  {
+    "revision": "6997f94a94b8527dc63b",
+    "url": "/static/js/runtime-main.9338123e.js"
+  }
+]);
